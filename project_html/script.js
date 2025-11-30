7f08d325836e53341114ec91f92ca00f1a34e3b9
Mahulak <prlukas.vaculik72@gmail.com>
2025-11-30 15:06:35 +0100

    html_projekt

---
+++ project_html/script.js
@@ -0,0 +1,7 @@
+        function zmenBarvy() {
+            var blokObsah = document.querySelector(".obsah");
+            blokObsah.classList.toggle("svetly-rezim");
+        }
+        function vyskocAlert() {
+            alert("Ahoj! Vítej na mé stránce!");
+        }
